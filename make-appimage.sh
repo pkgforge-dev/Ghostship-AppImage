67c36ff2e5d9cc6cd7a17ed56e10fba54eed269a
Link4Electronics <link4electronics@gmail.com>
2026-02-08 09:45:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,12 +9,12 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/ghostship.png
-export DESKTOP=/usr/share/applications/ghostship.desktop
+#export DESKTOP=/usr/share/applications/ghostship.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-mkdir -p ./AppDir/bin
-mv /opt/ghostship/* ./AppDir/bin
+#mkdir -p ./AppDir/bin
+#mv /opt/ghostship/* ./AppDir/bin
 #mv /opt/ghostship/ghostship.o2r ./AppDir/bin
 #mv /opt/ghostship/config.yml ./AppDir/bin
 #mv /opt/ghostship/gamecontrollerdb.txt ./AppDir/bin
